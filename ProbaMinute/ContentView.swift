70d16f4adca808bf40e2fd147550a1196bb42430
Ivan Dimitrov <biobeehive@abv.bg>
2020-11-04 06:58:37 +0200

    Initial Commit

---
+++ ProbaMinute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProbaMinute
+//
+//  Created by Ivan Dimitrov on 4.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProbaMinute/ProbaMinuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProbaMinuteApp.swift
+//  ProbaMinute
+//
+//  Created by Ivan Dimitrov on 4.11.20.
+//
+
+import SwiftUI
+
+@main
+struct ProbaMinuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
